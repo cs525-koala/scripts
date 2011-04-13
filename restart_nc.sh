18f6ecab4d2fb699f5f2cb18b59ebf41b47cb94f
Will Dietz <wdietz2@illinois.edu>
2011-04-13 16:40:46 -0500

    Break apart the deploy and restart of nc's

--- restart_nc.sh
+++ restart_nc.sh
@@ -1,22 +1,10 @@
-#!/bin/sh -e
+#!/bin/sh
+# To be run on each nc that to be bounced...
 
-#TODO: Verify that EUCALYPTUS is set.
-#TODO: Make sure the running user is root.
-#TODO: This should be run from the eucalyptus source tree!
+# Stop any running NC
+$EUCALYPTUS/etc/init.d/eucalyptus-nc stop
 
-make install
-
-OTHERS=""
-OTHERS="$OTHERS cn72.cloud.cs.illinois.edu"
-OTHERS="$OTHERS cn73.cloud.cs.illinois.edu"
-OTHERS="$OTHERS cn74.cloud.cs.illinois.edu"
-
-# Rsync updated $EUCALYPTUS tree to other nodes
-for host in $OTHERS; do
-  rsync -a $EUCALYPTUS/ root@$host:$EUCALYPTUS/
-done
-
-#
+# Reconfigure and make sure permissions are peachy...
 $EUCALYPTUS/usr/sbin/euca_conf -d $EUCALYPTUS --hypervisor kvm --instances /vm_images --user eucalyptus --setup --enable cloud --enable walrus --enable sc
 chown eucalyptus -R $EUCALYPTUS
 $EUCALYPTUS/usr/sbin/euca_conf -d $EUCALYPTUS --hypervisor kvm --instances /vm_images --user eucalyptus --setup --enable cloud --enable walrus --enable sc
@@ -25,5 +13,5 @@ $EUCALYPTUS/usr/sbin/euca_conf -d $EUCALYPTUS --setup
 # Change 'xenbr0' to 'br0' in the config.
 sed -i s/xenbr0/br0/ $EUCALYPTUS/etc/eucalyptus/eucalyptus.conf
 
-# Any other changes?
-$EUCALYPTUS/etc/init.d/eucalyptus-nc restart
+# Finally, bounce the nc!
+$EUCALYPTUS/etc/init.d/eucalyptus-nc start
