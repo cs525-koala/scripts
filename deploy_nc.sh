fe6f8ad558c2e872915e4db4bb95244a06521a1f
Will Dietz <wdietz2@illinois.edu>
2011-04-13 18:56:57 -0500

    More script improvement goodness, seems to work nc-deploying-wise.

--- deploy_nc.sh
+++ deploy_nc.sh
@@ -3,9 +3,13 @@
 #TODO: This should be run from the eucalyptus source tree!
 
 # Figure out where we really are
+SRC=$PWD
 CANON=`readlink -f $0`
 DIR=`dirname $CANON`
 cd $DIR
+
+# Permissions?!
+SHARE=/project/IKB-341568
 
 # Setup environment variables
 . ./setup_vars
@@ -16,23 +20,50 @@ if [[ $UID -ne 0 ]]; then
   exit 1
 fi
 
-SCRIPTS=$EUCALYPTUS/scripts
-
-#make install
-
 OTHERS=""
 OTHERS="$OTHERS cn72.cloud.cs.illinois.edu"
 OTHERS="$OTHERS cn73.cloud.cs.illinois.edu"
 OTHERS="$OTHERS cn74.cloud.cs.illinois.edu"
 
+SCRIPTS=$EUCALYPTUS/scripts
+
+make install -C $SRC
+
+# Change 'xenbr0' to 'br0' in the config.
+echo "Fixing xenbr0 -> br0 in eucalyptus.conf..."
+sed -i "s/xenbr0/br0/" $EUCALYPTUS/etc/eucalyptus/eucalyptus.conf || echo "Search/Replace failed, probably already fixed..."
+
+# Reconfigure and make sure permissions are peachy...
+$EUCALYPTUS/usr/sbin/euca_conf -d $EUCALYPTUS --hypervisor kvm --instances $SHARE --user eucalyptus --setup --enable cloud --enable walrus --enable sc
+chown eucalyptus -R $EUCALYPTUS
+$EUCALYPTUS/usr/sbin/euca_conf -d $EUCALYPTUS --hypervisor kvm --instances $SHARE --user eucalyptus --setup --enable cloud --enable walrus --enable sc
+$EUCALYPTUS/usr/sbin/euca_conf -d $EUCALYPTUS --setup
+
+# Stop all the nc's
+CMD="$EUCALYPTUS/etc/init.d/eucalyptus-nc stop"
+for host in $OTHERS; do
+  echo "Stopping NC on $host..."
+  ssh root@$host $CMD || echo "Stopping failed, assuming because already stopped..."
+done
+# (Locally as well)
+echo "Stopping local NC"
+$CMD || echo "Stopping failed, assuming because already stopped..."
 
 # Rsync updated $EUCALYPTUS tree to other nodes
 for host in $OTHERS; do
-  rsync --progress -e ssh -av $EUCALYPTUS/ root@$host:$EUCALYPTUS/
+  echo "Syncing with $host..."
+  rsync --progress -e ssh -av $EUCALYPTUS/ root@$host:$EUCALYPTUS/ \
+    --exclude var/log \
+    --exclude var/run
 done
 
 # Okay now restart the nc's on those hosts...
+CMD="$EUCALYPTUS/etc/init.d/eucalyptus-nc start"
 for host in $OTHERS; do
-  ssh eucalyptus@$host $SCRIPTS/restart_nc.sh
+  echo "Starting NC on $host..."
+  ssh root@$host $CMD
 done
+# (Locally as well)
+echo "Starting local NC"
+$CMD
 
--- restart_nc.sh
+++ restart_nc.sh
@@ -13,10 +13,10 @@ cd $DIR
 $EUCALYPTUS/etc/init.d/eucalyptus-nc stop
 
 # Reconfigure and make sure permissions are peachy...
+$EUCALYPTUS/usr/sbin/euca_conf -d $EUCALYPTUS --setup
 $EUCALYPTUS/usr/sbin/euca_conf -d $EUCALYPTUS --hypervisor kvm --instances /vm_images --user eucalyptus --setup --enable cloud --enable walrus --enable sc
 chown eucalyptus -R $EUCALYPTUS
 $EUCALYPTUS/usr/sbin/euca_conf -d $EUCALYPTUS --hypervisor kvm --instances /vm_images --user eucalyptus --setup --enable cloud --enable walrus --enable sc
-$EUCALYPTUS/usr/sbin/euca_conf -d $EUCALYPTUS --setup
 
 # Change 'xenbr0' to 'br0' in the config.
 sed -i s/xenbr0/br0/ $EUCALYPTUS/etc/eucalyptus/eucalyptus.conf
