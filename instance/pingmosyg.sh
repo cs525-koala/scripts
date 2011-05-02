16866ce09d597f1428113b6334029d2842be6511
Will Dietz <wdietz2@illinois.edu>
2011-05-02 18:10:57 -0500

    instance/*: Comment and touchup the scripts.

--- instance/pingmosyg.sh
+++ instance/pingmosyg.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+# Fork off a thread that pings mosyg roughly every second.
+# This keeps traffic FROM the node, so no matter where it is
+# (say, after it just got migrated)
+# the nc, the switch it's on, etc, all update accordingly.
+
+# TODO: Be smarter and only do this kind of thing after
+#       a migration, perhaps rig the nc to do it for us.
+# TODO: It's possible libvirt/kvm/qemu does some of this already
+#       (I didn't test extensively enough to find out)
 (while [ 1 ];
 do
-	ping -c1 -s1 mosyg.cs.uiuc.edu
-	sleep 1
+  ping -c1 -s1 mosyg.cs.uiuc.edu
+  sleep 1
 done) 2>&1 > /dev/null &
--- instance/setip.sh
+++ instance/setip.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
+# Go through the cmdlines (often taking form key=value)
+# Exporting them to our environment, ignoring errors.
+# TODO: Make this less hacky and actually parse out
+# the 'MYIP=IP' part
+# However, this works for now.
 for i in `cat /proc/cmdline`;
 do
-	export $i
+  export $i
 done
 
+# Use the MYIP boot parameter to configure our network
 ifconfig eth0 $MYIP
 
+# Default gw on the CCT, from cn71
 route add default gw 172.22.28.7
 
+# Some DNS goodness to get us going, mostly from cn71
 echo "search cloud.cs.illinois.edu" > /etc/resolv.conf
 echo "nameserver 128.174.252.4" >> /etc/resolv.conf
 echo "nameserver 128.174.252.5" >> /etc/resolv.conf
