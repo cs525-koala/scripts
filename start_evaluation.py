d986206caa94646fdfad68afb3299b9ad2f28a5b
Will Dietz <wdietz2@illinois.edu>
2011-05-03 15:38:05 -0500

    updates to push butan script

--- start_evaluation.py
+++ start_evaluation.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
 import sys, re, time, subprocess
+hadoop_home = "/usr/lib/hadoop"
 
 #even though it is incorrect, we will accept numbers 256-999 for the sake of simplicity
-ip_re = re.compile(".*(\d{1,3}:\d{1,3}:\d{1,3}:\d{1,3}).*")
+ip_re = re.compile("(\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3})")
 
-vm_startup_timeout = 300 #(5 min)
+vm_startup_timeout = 3#00 #(5 min)
 second_job_start_time = 1800 #(30 min)
 third_job_start_time = 3600 #(60 min)
 
@@ -15,20 +16,50 @@ hadoop_instance_start_command = "echo 'starting hadoop instance'"
 ip_list = []
 
 def isIP(potential_ip):
-    if None == ip_re.match(potential_ip):
+    if None == ip_re.search(potential_ip):
         return False
     else: 
         return True
 
-def getIP(potential_ip):
-    match = ip_re.match(potential_ip)
+def getIPstring(potential_ip):
+    match = ip_re.search(potential_ip)
     if match == None:
         return None
     else:
         return match.group(1)
 
 
+def printOutput(proc):
+    (proc_stdout, proc_stderr) = (proc.stdout, proc.stderr)
+    for line in proc_stdout:
+        print line
+    for line in proc_stderr:
+        print "error: ", line
+
+def getIPs():
+    current_ip_list = []
+    ip_proc = subprocess.Popen("ls /opt/hadoop_slave_ips", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    (proc_stdout, proc_stderr) = (ip_proc.stdout, ip_proc.stderr)
+    for line in proc_stdout:
+        if isIP(line):
+            current_ip_list.append(getIPstring(line))
+            print "ip found: ", getIPstring(line)
+        else:
+            print "invalid ip: ", line
+    for line in proc_stderr:
+        print "error: ", line
+    return current_ip_list
+
+
 #MAIN EXECUTION
+
+#clear old data
+#  clear old ip files <disabled for testing>
+#rm_ipfile_proc = subprocess.Popen("rm /opt/hadoop_slave_ips/*")
+#  clear old slaves file
+clear_proc = subprocess.Popen("echo '' > " + hadoop_home + "/hadoop/conf/slaves", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+printOutput(clear_proc)
+
 
 #get time
 start_time = time.gmtime()
@@ -37,11 +68,7 @@ start_time = time.gmtime()
 for count in range(hadoop_instance_count):
     print "starting hadoop instance ", count
     hadoop_start_proc = subprocess.Popen(hadoop_instance_start_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-    (sub_stdout, sub_stderr) = (hadoop_start_proc.stdout, hadoop_start_proc.stderr)
-    for line in sub_stdout:
-        print line
-    for line in sub_stderr:
-        print "error: ", line
+    printOutput(hadoop_start_proc)
 
 
 #collect ips for vm_startup_timeout time
@@ -50,33 +77,26 @@ time.sleep(vm_startup_timeout)
 
 #  note that the vms will copy a file with their ip address to /opt/hadoop_slave_ips
 #  parse these and add the ips to a list
-ip_proc = subprocess.Popen("ls /opt/hadoop_slave_ips", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-(sub_stdout, sub_stderr) = (ip_proc.stdout, ip_proc.stderr)
-for line in sub_stdout:
-    if isIP(line):
-        ip_list.append(getIP(line))
-        print "ip found: ", getIP(line)
-    else:
-        print "invalid ip: ", line
-for line in sub_stderr:
-    print "error: ", line
+print "finding slave ips"
+ip_list = getIPs()
 
-#  clear old slaves file
-clear_proc = subprocess.Popen("echo '' > /usr/lib/hadoop/conf/slaves")
-
-#  put those ips in the /usr/lib/hadoop/conf/slaves file
+#  put those ips in the hadoop_home/conf/slaves file
 for ip in ip_list:
-    ip_cat_proc = subprocess.Popen("echo'" + ip + "' >> /usr/lib/hadoop/conf/slaves", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-    (sub_stdout, sub_stderr) = (ip_cat_proc.stdout, ip_cat_proc.stderr)
-    for line in sub_stdout:
-        print line
-    for line in sub_stderr:
-        print "error: ", line
+    ip_cat_proc = subprocess.Popen("echo '" + ip + "' >> " + hadoop_home + "/conf/slaves", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    printOutput(ip_cat_proc)
 
-
+#  copy slaves file to the instances
+for ip in ip_list:
+    print "copying slaves file to: ", ip
+    scp_slave_proc = subprocess.Popen("scp " + hadoop_home + "/conf/slaves " + ip + ":" + hadoop_home + "/conf/slaves", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    printOutput(scp_slave_proc)
 
 #launch hadoop job
-#  first setup slave ips
+hadoop_init_proc = subprocess.Popen(hadoop_home + "/restart_stuff.sh", shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+hadoop_init_proc.communicate("Y")
+printOutput(hadoop_init_proc)
+
+hadoop_job_proc = subprocess.Popen(hadoop_home + "/bin/hadoop jar " + hadoop_home + "
 
 
 
