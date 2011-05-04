1745e71dfc4e311a3a187b0cbea8ee82f2c10355
Will Dietz <wdietz2@illinois.edu>
2011-05-04 17:52:22 -0500

    virsh_monitor: note about key access prereq

--- virsh_monitor.sh
+++ virsh_monitor.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Run this like 'watch -n2 ./virsh_monitor.sh' to monitor the VM's and where they actually are.
+# Assumes public-key ssh access as root to the hosts.
 
 HOSTS="cn71 cn72 cn73 cn74"
 for i in $HOSTS;
