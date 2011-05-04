6ec9e509d6e089c33f2a36ae812b4250d644f30a
Kevin Larson <kevinlarson1@gmail.com>
2011-05-03 20:34:27 -0500

    ton of improvements, most specifically, integration with euca2ools

--- start_evaluation.py
+++ start_evaluation.py
@@ -10,10 +10,15 @@ vm_startup_timeout = 3#00 #(5 min)
 second_job_start_time = 1800 #(30 min)
 third_job_start_time = 3600 #(60 min)
 
-hadoop_instance_count = 8
-hadoop_instance_start_command = "echo 'starting hadoop instance'"
+vm_emi = "emi-a8720fe6"
 
-ip_list = []
+hadoop_instance_count = 1
+#"instance" refers to a unique instance id representing the instance
+hadoop_instance_list = [] 
+hadoop_ip_list = []
+
+global_instance_list = []
+
 
 def isIP(potential_ip):
     if None == ip_re.search(potential_ip):
@@ -28,7 +33,6 @@ def getIPstring(potential_ip):
     else:
         return match.group(1)
 
-
 def printOutput(proc):
     (proc_stdout, proc_stderr) = (proc.stdout, proc.stderr)
     for line in proc_stdout:
@@ -36,67 +40,142 @@ def printOutput(proc):
     for line in proc_stderr:
         print "error: ", line
 
-def getIPs():
-    current_ip_list = []
-    ip_proc = subprocess.Popen("ls /opt/hadoop_slave_ips", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+def subprocWrapper(command):
+    return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+
+#also updates global_instance_list
+def getNewIPs():
+    new_ip_list = []
+    ip_proc = subprocWrapper("euca-describe-instances")
     (proc_stdout, proc_stderr) = (ip_proc.stdout, ip_proc.stderr)
     for line in proc_stdout:
-        if isIP(line):
-            current_ip_list.append(getIPstring(line))
-            print "ip found: ", getIPstring(line)
+        word_list = line.split()
+        if len(word_list) > 1 and word_list[0] == "INSTANCE":
+            print "ip: ", word_list[3], " found for instance: ", word_list[1] 
+            if word_list[1] not in global_instance_list:
+                new_ip_list.append(word_list[3])
+                global_instance_list.append(word_list[1])
         else:
-            print "invalid ip: ", line
+            print "not an instance: ", line
     for line in proc_stderr:
         print "error: ", line
-    return current_ip_list
+    return new_ip_list
+
+#must be called on the output of a subprocess.Popen of euca-run-instances
+def getInstanceIds(proc):
+    (proc_stdout, proc_stderr) = (proc.stdout, proc.stderr)
+    new_instance_list = []
+    for line in proc_stdout:
+        word_list = line.split()
+        if len(word_list) > 1 and word_list[0] == "INSTANCE":
+            print "found id: ", word_list[1], " in: ", line
+            new_instance_list.append(word_list[1])
+        else:
+            print "not an instance: ", line
+    for line in proc_stderr:
+        print line
+    return new_instance_list
+
+def hadoopInit():
+    hadoop_stop_cmd = hadoop_home + "/bin/stop-all.sh"
+    hadoop_stop_proc = subprocWrapper(hadoop_stop_cmd)
+    printOutput(hadoop_stop_proc)
+    
+    hadoop_clear_hdfs_cmd = "rm -rf " + hadoop_home + "/hdfs/*"
+    hadoop_clear_hdfs_proc = subprocWrapper(hadoop_clear_hdfs_cmd)
+    printOutput(hadoop_clear_hdfs_proc)
+
+    hadoop_clear_tmp_cmd = "rm -rf " + hadoop_home + "/tmp/*"
+    hadoop_clear_tmp_proc = subprocWrapper(hadoop_clear_tmp_cmd)
+    printOutput(hadoop_clear_tmp_proc)
+
+    hadoop_format_cmd = "echo 'Y' | " + hadoop_home + "/bin/hadoop namenode -format"
+    hadoop_format_proc = subprocWrapper(hadoop_format_cmd)
+    printOutput(hadoop_format_proc)
+
+    hadoop_start_cmd = hadoop_home + "/bin/start-all.sh"
+    hadoop_start_proc = subprocWrapper(hadoop_start_cmd)
+    printOutput(hadoop_start_proc)
+
 
 
 #MAIN EXECUTION
 
-#clear old data
-#  clear old ip files <disabled for testing>
-#rm_ipfile_proc = subprocess.Popen("rm /opt/hadoop_slave_ips/*")
-#  clear old slaves file
-clear_proc = subprocess.Popen("echo '' > " + hadoop_home + "/hadoop/conf/slaves", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+#clear old slaves file
+
+clear_proc_command = "echo '' > " + hadoop_home + "/conf/slaves"
+clear_proc = subprocWrapper(clear_proc_command)
 printOutput(clear_proc)
 
 
+
 #get time
+
 start_time = time.gmtime()
 
-#start hadoop vm's
-for count in range(hadoop_instance_count):
-    print "starting hadoop instance ", count
-    hadoop_start_proc = subprocess.Popen(hadoop_instance_start_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-    printOutput(hadoop_start_proc)
+
+
+#start hadoop vm's & record instance ids 
+
+print "starting ", hadoop_instance_count, " hadoop VMs"
+hadoop_instance_start_command = "euca-run-instances -n " + str(hadoop_instance_count)+ " " + vm_emi + " -t c1.medium"
+print "running: ", hadoop_instance_start_command
+hadoop_start_proc = subprocWrapper(hadoop_instance_start_command)
+hadoop_instance_list = getInstanceIds(hadoop_start_proc)
+global_instance_list.extend(hadoop_instance_list)
+
 
 
 #collect ips for vm_startup_timeout time
+
 #  first, sleep for that length
+print "allowing vms ", vm_startup_timeout, " seconds to start"
 time.sleep(vm_startup_timeout)
 
-#  note that the vms will copy a file with their ip address to /opt/hadoop_slave_ips
-#  parse these and add the ips to a list
-print "finding slave ips"
-ip_list = getIPs()
+
+
+#  parse euca-describe-instances for new ips
+print "getting slave ips from euca-describe-instances"
+hadoop_ip_list = getNewIPs()
+print hadoop_ip_list
+
 
 #  put those ips in the hadoop_home/conf/slaves file
-for ip in ip_list:
-    ip_cat_proc = subprocess.Popen("echo '" + ip + "' >> " + hadoop_home + "/conf/slaves", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+print "generating slaves file from hadoop instances' ips"
+for ip in hadoop_ip_list:
+    ip_cat_cmd = "echo '" + ip + "' >> " + hadoop_home + "/conf/slaves"
+    ip_cat_proc = subprocWrapper(ip_cat_cmd)
     printOutput(ip_cat_proc)
 
+print "hadoop instances:"
+for instance in hadoop_instance_list:
+    print instance
+
+print "global instances:"
+for instance in global_instance_list:
+    print instance
+
+for instance in hadoop_instance_list:
+    print "killing instance: ", instance
+    instance_kill_proc = subprocWrapper("euca-terminate-instances " + instance)
+    printOutput(instance_kill_proc)
+'''
 #  copy slaves file to the instances
 for ip in ip_list:
     print "copying slaves file to: ", ip
-    scp_slave_proc = subprocess.Popen("scp " + hadoop_home + "/conf/slaves " + ip + ":" + hadoop_home + "/conf/slaves", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    scp_slave_cmd = "scp " + hadoop_home + "/conf/slaves " + ip + ":" + hadoop_home + "/conf/slaves"
+    scp_slave_proc = subprocWrapper(scp_slave_cmd)
     printOutput(scp_slave_proc)
 
-#launch hadoop job
-hadoop_init_proc = subprocess.Popen(hadoop_home + "/restart_stuff.sh", shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-hadoop_init_proc.communicate("Y")
-printOutput(hadoop_init_proc)
+#set up hadoop
+print "initializing hadoop"
+hadoopInit()
 
-hadoop_job_proc = subprocess.Popen(hadoop_home + "/bin/hadoop jar " + hadoop_home + "
+#launch hadoop job
+hadoop_randomwriter_job_string = hadoop_home + "/bin/hadoop jar " + hadoop_home + "/hadoop-0.20.2-examples.jar randomwriter random_data"
+print "running hadoop job, with command: ", hadoop_randomwriter_job_string
+hadoop_job_proc = subprocWrapper(hadoop_randomwriter_job_string)
+printOutput(hadoop_job_proc)
 
 
 
@@ -104,3 +183,4 @@ hadoop_job_proc = subprocess.Popen(hadoop_home + "/bin/hadoop jar " + hadoop_hom
 
 
 #after third_job_start_time has elapsed, start third job
+'''
