77252a18288588b701f1917c073b92ccfcf5ddc1
Will Dietz <wdietz2@illinois.edu>
2011-05-04 17:47:56 -0500

    Add my little 'virsh_monitor' script.

---
+++ virsh_monitor.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Run this like 'watch -n2 ./virsh_monitor.sh' to monitor the VM's and where they actually are.
+
+HOSTS="cn71 cn72 cn73 cn74"
+for i in $HOSTS;
+do
+  (ssh root@$i virsh list) | grep -v "Not running" | grep -v "\-\-\-" | grep -v "Id Name" |grep -v "^$" | awk "{printf \"($i): %s\n\", \$0}"
+done
+
