5f051e52691a8a5da9251daad1dde19574e323fb
Will Dietz <wdietz2@illinois.edu>
2011-05-05 06:04:15 -0500

    terminate_all: Forget 5 at a time, kill all in single invocation.

--- terminate_all.sh
+++ terminate_all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Archiving the command I used to tell euc to nuke all instances, jic it's useful
-python `which euca-describe-instances` |grep i-|cut -f2|xargs -n5 python `which euca-terminate-instances`
+python `which euca-describe-instances` |grep i-|cut -f2|xargs python `which euca-terminate-instances`
