3d46bb6bd1688e90cafff2a73047a5d5591d7633
Kevin Larson <kevinlarson1@gmail.com>
2011-05-07 02:12:40 -0500

    redid the dict and improved time values

--- cpu_benchmark.py
+++ cpu_benchmark.py
@@ -2,21 +2,21 @@
 
 #It is assumed that this script is run only on mosyg and only as root
 
-import sys, re, time, subprocess, ping, socket, threading
+import sys, re, datetime, time, subprocess, ping, socket, threading
 
 run_without_eucalyptus = False
 
-start_time_multiplier = 25
+start_time_multiplier = 5
 # start pos, [instance number, duration (exponent)]
-cpu_task_values = dict(((0, [4,23]),
-                        (1, [8,23]),
+cpu_task_values = dict(((0, [1,23]),
+                        (1, [2,23]),
                         (2, [3,23]),
-                        (3, [6,23]),
-                        (4, [9,23]),
-                        (5, [7,23]),
-                        (6, [1,23]),
-                        (7, [5,23]),
-                        (8, [2,23])))
+                        (3, [4,23]),
+                        (4, [5,23]),
+                        (5, [6,23]),
+                        (6, [7,23]),
+                        (7, [8,23]),
+                        (8, [9,23])))
                         
 def prettyTime():
     return time.strftime('%x: %X', time.gmtime())
@@ -32,13 +32,16 @@ def subprocWrapper(command):
     return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
 
 def cpu_task(instance_number, exponent):
-    print "starting job with exponent", exponent, "on hadoop"+str(instance_number)
+    task_start_time = prettyTime()
+    print "\nAt ", task_start_time, "starting job with exponent", exponent, "on hadoop"+str(instance_number)
     cpu_task_cmd = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no hadoop" + str(instance_number) + " \"echo '2^2^" + str(exponent) + "' | bc >/dev/null\""
     if run_without_eucalyptus:
         print "debug mode, running: " + cpu_task_cmd
     else:
         cpu_task_proc = subprocWrapper(cpu_task_cmd)
         printOutput(cpu_task_proc)
+        task_end_time = prettyTime()
+        print "at", task_end_time, "the cpu task ended on hadoop" + str(instance_number)
 
 # MAIN EXECUTION
 
