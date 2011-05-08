235acbb4db54df213ba27ba10aae194142bc839f
Will Dietz <wdietz2@illinois.edu>
2011-05-08 01:13:17 -0500

    cpu_driver.sh: Fix script >_> (Approved by Kevin Larson <kevinlarson1@gmail.com>)

--- cpu_driver.sh
+++ cpu_driver.sh
@@ -7,11 +7,10 @@
 
 #TODO: Increase offset times in cpu_benchmark.py? (LATER!)
 
-ITERS=12
+ITERS=2
 
 RESULTS_FILE=/opt/cpu_bench_times
 
-KILL_RAEP_CMD="ps aux|grep raep|awk '{print \\\$2}'|xargs echo kill -9"
 RAEPCMD="nohup /opt/cpu_raep.sh >& /dev/null < /dev/null &"
 BUNDLECMD="tar cvf $RESULTS_FILE.$UNIQ.tar $RESULTS_FILE.$UNIQ.*"
 
@@ -20,7 +19,7 @@ UNIQ="CPU$$"
 echo "This run's uniq identifier is **$UNIQ**"
 
 # Comment this out to actually execute commands
-RUN=echo
+#RUN=echo
 
 # Runs the requested command on the requested host, 'safely'
 function remoterun() {
@@ -42,29 +41,60 @@ function remoterunall() {
     done
 }
 
+# Main Execution
+
+# Provision the hosts with our killgrep command...
+for i in `seq 1 9`
+do
+    scp $PWD/killgrep.sh root@hadoop$i: >& /dev/null
+done
+scp $PWD/killgrep.sh cn72: >& /dev/null
+scp $PWD/killgrep.sh cn73: >& /dev/null
+
 # Make sure no rape tasks are running on the nc's...
-remoterun cn72 "$KILL_RAEP_CMD"
-remoterun cn73 "$KILL_RAEP_CMD"
+remoterun cn72 "./killgrep.sh cpu_raep" > /dev/null
+remoterun cn72 "./killgrep.sh bc" > /dev/null
+
+remoterun cn73 "./killgrep.sh cpu_raep" > /dev/null
+remoterun cn73 "./killgrep.sh bc" > /dev/null
 
 
 # Main loop
 # Run each iteration of the eval, copy over the output
 # And increase the number of raep processes on the nc's
-for i in `seq 1 $ITERS`
+for ITER in `seq 1 $ITERS`
 do
-    echo "ITERATION $i..."
+    echo "ITERATION $ITER..."
+
+    # Make sure the instances are clean
+    remoterunall "./killgrep.sh cpu_bench" > /dev/null
+    remoterunall "./killgrep.sh cpu_task" > /dev/null
+    remoterunall "./killgrep.sh bc" > /dev/null
 
     # Kick off the tasks
     $RUN ./cpu_benchmark.py
-    sleep 3
 
     # Copy results to file for this iteration...
-    remoterunall "cp $RESULTS_FILE $RESULTS_FILE.$UNIQ.$i"
+    remoterunall "cp $RESULTS_FILE $RESULTS_FILE.$UNIQ.$ITER" > /dev/null
 
     # Done! Now run another raeps...
-    remoterun cn72 $RAEPCMD
-    remoterun cn73 $RAEPCMD
+    remoterun cn72 "$RAEPCMD" > /dev/null
+    remoterun cn73 "$RAEPCMD" > /dev/null
 done
 
 # Bundle up the results for easier scp'ing later.
-remoterunall $BUNDLECMD
+#remoterunall $BUNDLECMD
+
+# Copy results and commit them.
+RES_DIR=/opt/results/eval-$UNIQ/
+mkdir -p $RES_DIR
+for i in `seq 1 9`
+do
+    mkdir -p $RES_DIR/$i
+    scp hadoop$i:"$RESULTS_FILE.$UNIQ*" $RES_DIR/$i/
+done
+
+cd /opt/results
+git add $RES_DIR
+git commit -a -m "Automatic commit of eval-$UNIQ"
+git push origin master
